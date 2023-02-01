6abd505139419e89d58297058f48c2204d25352e
allan-lafrance <115147533+allan-lafrance@users.noreply.github.com>
2023-02-01 11:03:41 +0100

    Update password.py

--- password.py
+++ password.py
@@ -2,26 +2,26 @@ import hashlib
 import json
 
 def password_checker(password):
-    # Vérification de la longueur du mot de passe
+
     if len(password) < 8:
         return False
-    # Vérification de l'existence d'au moins une lettre majuscule
+
     if not any(char.isupper() for char in password):
         return False
-    # Vérification de l'existence d'au moins une lettre minuscule
+
     if not any(char.islower() for char in password):
         return False
-    # Vérification de l'existence d'au moins un chiffre
+
     if not any(char.isdigit() for char in password):
         return False
-    # Vérification de l'existence d'au moins un caractère spécial
+
     if not any(char in "!@#$%^&*" for char in password):
         return False
-    # Si toutes les vérifications ont été passées, le mot de passe est valide
+
     return True
 
 def hash_password(password):
-    # Cryptage du mot de passe avec l'algorithme SHA-256
+
     return hashlib.sha256(password.encode()).hexdigest()
 
 def add_password(passwords):
